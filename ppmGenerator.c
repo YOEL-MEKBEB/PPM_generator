6b1076fcbadb6acaeb146bf141edbbc9e5d82028
Yoel Mekbeb <mekbe003@umn.edu>
2025-01-27 13:50:21 -0600

    error checking

--- ppmGenerator.c
+++ ppmGenerator.c
@@ -2,8 +2,12 @@
 
 int writeHeader(FILE* file, char* width, char* height){
     char* p3 = "P3\n";
-    fwrite(p3, strlen(p3), 1, file);
+    if(fwrite(p3, strlen(p3), 1, file) < 1){
+        perror("failed to write header\n");
+        return -1;
+    }
 
+    // concatenates all the strings together to make the header for the ppm file
     char widthAndHeight[30] = "";
     char* resultString = strcat(widthAndHeight, width);
     resultString = strcat(widthAndHeight, " ");
@@ -12,7 +16,11 @@ int writeHeader(FILE* file, char* width, char* height){
     resultString = strcat(widthAndHeight, "255");
     resultString = strcat(widthAndHeight, "\n");
 
-    fwrite(resultString, strlen(resultString), 1, file);
+    //writes the entire string to the file provided to the function
+    if(fwrite(resultString, strlen(resultString), 1, file) < 1){
+        perror("failed to write header\n");
+        return -1; 
+    }
 
     return 0;
 }
@@ -24,17 +32,33 @@ int writeRandomBody(FILE* file, char* width, char* height){
     printf("%d\n", numWidth);
     printf("%d\n", numHeight);
 
-     for(int i = 0; i < numHeight; i++){
+    // loops through the size of the image to set each pixel value and write to the file
+    for(int i = 0; i < numHeight; i++){
         int num;
         char numChar[5];
+
+        // j < 3 * numWidth-1 because each pixel will have 3 number associated with it
+        // and the the last pixel will have newline concatenated instead of space.
         for(int j = 0; j < 3 * numWidth-1; j++){
-            num = rand()%256;
-            sprintf(numChar, "%d ", num);
-            fwrite(numChar, strlen(numChar), 1, file);
+            num = rand()%256; // 0 to 2555
+            if(sprintf(numChar, "%d ", num) == -1){
+                printf("sprintf failed in writeRandomBody\n");
+                return -1;
+            }
+            if(fwrite(numChar, strlen(numChar), 1, file) < 1){
+                perror("failed to write in writeRandomBody\n");
+                return -1; 
+            }
         }
-        sprintf(numChar, "%d\n", num);
-        fwrite(numChar, strlen(numChar), 1, file);
-     }
+        if(sprintf(numChar, "%d\n", num) == -1){
+            printf("sprintf failed in writeRandomBody\n");
+            return -1;
+        }
+        if(fwrite(numChar, strlen(numChar), 1, file) < 1){
+                perror("failed to write in writeRandomBody\n");
+                return -1; 
+        }
+    }
     return 0;
 }
 
@@ -45,18 +69,36 @@ int writeHorizontalGradient(FILE* file, char* width, char* height){
     printf("%d\n", numWidth);
     printf("%d\n", numHeight);
 
-     for(int i = 0; i < numHeight; i++){
+    // loops through the size of the image to set each pixel value and write to the file
+    for(int i = 0; i < numHeight; i++){
         int num;
         char numChar[5];
+
+        // j < 3 * numWidth-1 because each pixel will have 3 number associated with it
+        // and the the last pixel will have newline concatenated instead of space.
         for(int j = 0; j < 3 * numWidth-1; j++){
             num = j/3;
-            sprintf(numChar, "%d ", num);
-            fwrite(numChar, strlen(numChar), 1, file);
+            if(sprintf(numChar, "%d ", num) == -1){
+                printf("sprintf failed in writeHorizontalGradient\n");
+                return -1;
+            }
+            if(fwrite(numChar, strlen(numChar), 1, file) < 1){
+                perror("failed to write in writeHorizontalGradient\n");
+                return -1;  
+            }
         }
         num++;
-        sprintf(numChar, "%d\n", num);
-        fwrite(numChar, strlen(numChar), 1, file);
-     }
+
+        if(sprintf(numChar, "%d\n", num)==-1){
+            printf("sprintf failed in writeHorizontalGradient\n");
+            return -1;
+        }
+
+        if(fwrite(numChar, strlen(numChar), 1, file) < 1){
+                perror("failed to write in writeHorizontalGradient\n");
+                return -1;  
+        }
+    }
     return 0;
 }
 
@@ -67,18 +109,34 @@ int writeVerticalGradient(FILE* file, char* width, char* height){
     printf("%d\n", numWidth);
     printf("%d\n", numHeight);
 
-     for(int i = 0; i < numHeight; i++){
+    // loops through the size of the image to set each pixel value and write to the file
+    for(int i = 0; i < numHeight; i++){
         int num;
         char numChar[5];
+
+        // j < 3 * numWidth-1 because each pixel will have 3 number associated with it
+        // and the the last pixel will have newline concatenated instead of space.
         for(int j = 0; j < 3 * numWidth-1; j++){
             num = i;
-            sprintf(numChar, "%d ", num);
-            fwrite(numChar, strlen(numChar), 1, file);
+            if(sprintf(numChar, "%d ", num) == -1){
+            printf("sprintf failed in writeVerticalGradient\n");
+            return -1;
+        }
+            if(fwrite(numChar, strlen(numChar), 1, file) < 1){
+                perror("failed to write in writeVerticalGradient\n");
+                return -1;  
+            }
         }
         num++;
-        sprintf(numChar, "%d\n", num);
-        fwrite(numChar, strlen(numChar), 1, file);
-     }
+        if(sprintf(numChar, "%d\n", num) == -1){
+            printf("sprintf failed in writeVerticalGradient\n");
+            return -1;
+        }
+        if(fwrite(numChar, strlen(numChar), 1, file) < 1){
+                perror("failed to write in writeVerticalGradient\n");
+                return -1;  
+        }
+    }
     return 0;
 }
 
@@ -89,22 +147,20 @@ int writeDiagonalGradient(FILE* file, char* width, char* height){
     printf("%d\n", numWidth);
     printf("%d\n", numHeight);
 
+    // creates an array of the same size as the ppm to simplify the pixel value calculations
+    // not efficient for large file sizes
     int imageArray[numHeight][3*numWidth];
-    printf("hello");
 
+    //scaling factors to adjust pixel values in loop relative to the size of the image
     float xScalingFactor = 256.0/(numWidth);
     float yScalingFactor = 256.0/numHeight;
     printf("xScaling %f\n", xScalingFactor);
     printf("yScaling %f\n", yScalingFactor);
-    // if(xScalingFactor <= 0){
-    //     xScalingFactor = 1;
-    // }
-    // if(yScalingFactor <= 0){
-    //     yScalingFactor = 1;
-    // }
 
+    // loops through the size of the image to set each pixel value
     for(int i = 0; i < numHeight; i++){
         for(int j = 0; j < 3 * numWidth; j++){
+            // this method ensures that the pixel color value doesn't exceed 255
             if(i>=j/3){
                 imageArray[i][j] = (int)(yScalingFactor * i);
             }else{
@@ -114,19 +170,35 @@ int writeDiagonalGradient(FILE* file, char* width, char* height){
         }
      } 
 
+    // loops through the size of the image to write to the file
     for(int i = 0; i < numHeight; i++){
         int num;
         char numChar[5];
         num = i;
+
+        // j < 3 * numWidth-1 because each pixel will have 3 number associated with it
+        // and the the last pixel will have newline concatenated instead of space.
         for(int j = 0; j < 3 * numWidth-1; j++){
             num = imageArray[i][j];
-            sprintf(numChar, "%d ", num);
-            fwrite(numChar, strlen(numChar), 1, file);
+            if(sprintf(numChar, "%d ", num) == -1){
+                printf("sprintf failed in writeDiagonalGradient\n");
+                return -1;
+            }
+            if(fwrite(numChar, strlen(numChar), 1, file) < 1){
+                perror("failed to write in writeDiagonalGradient\n");
+                return -1;  
+            }
             
         }
         num = imageArray[i][3 * numWidth-1];
-        sprintf(numChar, "%d\n", num);
-        fwrite(numChar, strlen(numChar), 1, file);
+        if(sprintf(numChar, "%d\n", num) == -1){
+            printf("sprintf failed in writeDiagonalGradient\n");
+            return -1;
+        }
+        if(fwrite(numChar, strlen(numChar), 1, file) < 1){
+                perror("failed to write in writeDiagonalGradient\n");
+                return -1;  
+        }
        
      } 
 
@@ -161,7 +233,10 @@ int main() {
         }
 
         char buf2[50];
-        fread(buf2, 1, sizeof(buf2), txtFile);
+        if(fread(buf2, 1, sizeof(buf2), txtFile) < 0){
+            printf("failed to read");
+            return 1;
+        }
 
         
 
@@ -191,18 +266,40 @@ int main() {
         FILE* ppmFile = fopen(ppm, "w");
 
         
-        writeHeader(ppmFile, width, height);
+        if(writeHeader(ppmFile, width, height) == -1){
+            fclose(ppmFile);
+            printf("writeHeader failed\n");
+            return 1;
+
+        }
+        // calls the appropriate functions depending on txt file structure
         if(strcmp(type, "diagonalGrad") == 0){
-            writeDiagonalGradient(ppmFile, width, height);
+            if(writeDiagonalGradient(ppmFile, width, height) == -1){
+                fclose(ppmFile);
+                printf("writeDiagonalGradient failed\n");
+                return 1;
+            }
         }else if (strcmp(type, "rand") == 0)
         {
-            writeRandomBody(ppmFile, width, height);
+            if(writeRandomBody(ppmFile, width, height)==-1){
+                fclose(ppmFile);
+                printf("writeRandomBody failed\n");
+                return 1;
+            }
         }else if (strcmp(type, "horizontalGrad") == 0)
         {
-            writeHorizontalGradient(ppmFile, width, height);
+            if(writeHorizontalGradient(ppmFile, width, height) == -1){
+                fclose(ppmFile);
+                printf("writeHorizontalGradient failed\n");
+                return 1;
+            }
         }else if (strcmp(type, "verticalGrad") == 0)
         {
-            writeVerticalGradient(ppmFile, width, height);
+            if(writeVerticalGradient(ppmFile, width, height) == -1){
+                fclose(ppmFile);
+                printf("writeVerticalGradient failed\n");
+                return 1;
+            }
         }
         
 
--- ppmGenerator.h
+++ ppmGenerator.h
@@ -5,17 +5,22 @@
 
 // write the header of the ppm file given in the parameter
 // using the width and the height
+// returns 0 on success and -1 on failure
 int writeHeader(FILE* file, char* width, char* height);
 
 
 // writes the body content of the ppm file
+// returns 0 on success and -1 on failure
 int writeRandomBody(FILE* file,  char* width, char* height);
 
 // draws a horizontal gradient
+// returns 0 on success and -1 on failure
 int writeHorizontalGradient(FILE* file, char* width, char* height);
 
 // draws a vertical gradient
+// returns 0 on success and -1 on failure
 int writeVerticalGradient(FILE* file, char* width, char* height);
 
 // draws a diagonal gradient
+// returns 0 on success and -1 on failure
 int writeDiagonalGradient(FILE* file, char* width, char* height);
